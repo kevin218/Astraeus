d9ef5615a35de87eb58712b70ce373c2c5aac189
Kevin B. Stevenson <kbstevenson@gmail.com>
2022-04-11 14:56:15 -0400

    Update tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,17 +1,17 @@
 import pytest
-import my_package.example
+import astreus.example
 
 def test_type():
     """
     Test addition with float, int and strung.
     """
-    assert my_package.example.add_one(1) == 2
-    assert my_package.example.add_one(1.1) == 2.1
-    assert my_package.example.add_one('1') == '11'
+    assert astreus.example.add_one(1) == 2
+    assert astreus.example.add_one(1.1) == 2.1
+    assert astreus.example.add_one('1') == '11'
 
 def test_error():
     """
     Test addition with list.
     """
     with pytest.raises(TypeError):
-        my_package.example.add_one([1])
+        astreus.example.add_one([1])
